a6ace06adbb29b609a9cf81802e72bf6987a25c0
Laura Slingerland <laura.sling@gmail.com>
2017-06-24 15:12:00 -0600

    Checkpoint 5 AddressBloc: Completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,11 @@
+require_relative 'controllers/menu_controller'
+
+#4, create a new MenuController when AddressBloc starts
+menu = MenuController.new
+
+#5, use  system "clear" to clear the command line
+system "clear"
 puts "Welcome to AddressBloc!"
+
+#6, call main_menu to display the menu.
+menu.main_menu
