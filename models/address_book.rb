8381f1a22008aefb306b52f0091ec925d0678790
Laura Slingerland <laura.sling@gmail.com>
2017-06-24 17:52:08 -0600

    Checkpoint 7 AddressBloc: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,4 +30,28 @@ require "csv"
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
   end
+  def binary_search(name)
+    # #1 we save the index of the leftmost item in the array in a variable named lower, and the index of rightmost item in the array in upper
+     lower = 0
+     upper = entries.length - 1
+
+     # #2 we loop while our lower index is less than or equal to our upper index
+     while lower <= upper
+       # #3 we find the middle index by taking the sum of lower and upper and dividing it by two
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       # #4 we compare the name that we are searching for, name, to the name of the middle index, mid_name
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     # #5 if we divide and conquer to the point where no match is found, we return nil
+     return nil
+  end
 end
